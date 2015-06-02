e747e57235b5172cd613fe1bd520ed00f8ee3116
mamort <mamort@new-laptop.local>
2015-06-02 17:04:54 -0500

    initial version

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/mamort/image_transport_ws/build/test_results"
+export CMAKE_PREFIX_PATH="/home/mamort/image_transport_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mamort/image_transport_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/mamort/image_transport_ws/devel/lib:/home/mamort/image_transport_ws/devel/lib/x86_64-linux-gnu:/home/mamort/ros_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/mamort/ros_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/mamort/image_transport_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/mamort/image_transport_ws/devel/lib/pkgconfig:/home/mamort/image_transport_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/mamort/ros_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/mamort/ros_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/mamort/image_transport_ws/build"
+export PYTHONPATH="/home/mamort/image_transport_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mamort/image_transport_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/mamort/image_transport_ws/src:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/mamort/image_transport_ws/build/test_results"
